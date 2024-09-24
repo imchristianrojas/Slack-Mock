a8cb86666cdd155a6d966d68816d77452efed631
Christian Rojas <crojas0315@gmail.com>
2024-09-23 17:22:16 -0700

    fixed backend syntax issue

--- backend/src/workspaces.js
+++ backend/src/workspaces.js
@@ -1,17 +1,10 @@
-
 const db = require('./db');
 
 exports.getWorkSpaces = async (req, res) => {
   console.log('User object:', req.user);
-  // if (!req.user || !req.user.id) {
-  //   return res.status(401).json({message: 'User not authenticated'});
-  // }
   const userId = req.user.id;
   console.log('Fetching workspaces for user ID:', userId);
   const workSpaces = await db.selectWorkspaces(userId);
-  for (const workspace of workSpaces) {
-
-  //  console.log('These are the workspace names:',workspaceNames);
   res.status(200).json(workSpaces);
 };
 
@@ -34,4 +27,4 @@ exports.getWorkSpaceChannels = async (req, res) => {
     console.error('Error fetching channels:', error);
     res.status(500).json({ message: 'Internal server error' });
   }
-}};
+};
